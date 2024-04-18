a832cb8300fef8d2dd3ec0e5355a1fbce6957142
Dani <carlos99.cp88secundaria@gmail.com>
2024-04-18 22:10:37 +0200

    first commit

---
+++ curso.js
@@ -0,0 +1,13 @@
+//Crear una función en JavaScript que tome un array de números como entrada
+ //y devuelva un nuevo array que contenga solo los números pares del array original,
+//en el mismo orden en que aparecen en el original
+
+
+
+
+numero = 6;
+while (numero < 110) {
+    numero++;
+    document.write(numero)
+}
+
