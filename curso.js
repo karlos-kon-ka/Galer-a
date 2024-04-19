24116f09f14d2a4b2cf62662a74402aed5d927d2
Dani <carlos99.cp88secundaria@gmail.com>
2024-04-19 04:05:14 +0200

    commit

--- curso.js
+++ curso.js
@@ -1,13 +1,9 @@
-//Crear una función en JavaScript que tome un array de números como entrada
- //y devuelva un nuevo array que contenga solo los números pares del array original,
-//en el mismo orden en que aparecen en el original
-
-
-
-
-numero = 6;
-while (numero < 110) {
-    numero++;
-    document.write(numero)
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll(".animated-image");
+    images.forEach(image => {
+        image.addEventListener("animationend", function() {
+            this.style.animationPlayState = "paused"; // Detiene la animación después de que ocurre una vez
+        });
+    });
+});
 
